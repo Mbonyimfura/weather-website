d984afaddfa7342f31c082fbf6c7a2d4a7462edf
tresor shooter <mbonyimfura>
2022-07-18 07:34:23 -0700

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast=(latitude,longitude,callback)=>{
         }else if(body.error){
             callback('Unable to find location',undefined)
         }else{
-             callback(undefined,`It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out`)
+             callback(undefined,`It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. It is ${body.current.humidity}% humidity`)
         }
 
     })
